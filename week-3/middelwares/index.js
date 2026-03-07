58317836a99d4a09b786094a3fa6589cb5a980d3
Amartya <amartya.kumar2001@gmail.com>
2026-03-08 01:41:08 +0530

    feat: using global catches and json parsing middlewares

--- week-3/middelwares/index.js
+++ week-3/middelwares/index.js
@@ -2,48 +2,65 @@ const express = require("express")
 
 const app = express()
 
-function authenticateUser(req, res, next) {
+// function authenticateUser(req, res, next) {
 
-  const userName = req.headers.name
-  const password = req.headers.password
+//   const userName = req.headers.name
+//   const password = req.headers.password
 
-  if (userName !== "AK" || password !== "AK@123") {
-    res.status(402).json({
-      msg: "Invalid username or password"
-    })
-    return
-  } else {
-    next()
-  }
+//   if (userName !== "AK" || password !== "AK@123") {
+//     res.status(402).json({
+//       msg: "Invalid username or password"
+//     })
+//     return
+//   } else {
+//     next()
+//   }
 
-}
+// }
 
-function kidneysCheck(req, res, next) {
+// function kidneysCheck(req, res, next) {
 
-  const noOfKidneys = parseInt(req.query.n)
+//   const noOfKidneys = parseInt(req.query.n)
 
-  if (noOfKidneys !== 1 && noOfKidneys !== 2) {
-    res.status(402).json({
-      msg: "This aint human"
-    })
-    return
-  } else {
-    next()
-  }
-}
+//   if (noOfKidneys !== 1 && noOfKidneys !== 2) {
+//     res.status(402).json({
+//       msg: "This aint human"
+//     })
+//     return
+//   } else {
+//     next()
+//   }
+// }
 
 
-app.get("/", authenticateUser, kidneysCheck, (req, res) => {
 
-  const userName = req.headers.name
-  const noOfKidneys = parseInt(req.query.n)
+
+
+// for parsing body object into json string use this global middleware,it automattically gets aapplied to each routes
+
+
+app.use(express.json())
+
+
+
+app.post("/checkup", (req, res) => {
+
+  const kidneys = req.body.kidneys
+  const kidneysLength = kidneys.length
+
 
   res.status(200).json({
-    msg: "ALL OKAY",
-    alt: `The user ${userName} has ${noOfKidneys} kidneys`
+    msg: `ALL OKAY and you have ${kidneysLength} kidneys.`,
   })
 
 
+})
+
+// global catch --> in case of any errors in any routes this catches it and send a good msg to the client
+app.use(function (err, req, res, next) {
+  res.status(402).json({
+    msg: 'Something up with the server'
+  })
 })
 
 
