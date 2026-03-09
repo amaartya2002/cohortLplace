3f971e062124d4545953cb8728e64d589e05ce00
Amartya <amartya.kumar2001@gmail.com>
2026-03-10 01:31:58 +0530

    chore: did the third part of assignment - errorCountMiddleware

--- week-3/middelwares/middleware-assignment.js
+++ week-3/middelwares/middleware-assignment.js
@@ -2,7 +2,6 @@ const express = require("express")
 
 const app = express()
 
-let requestCount = 0;
 
 /**
    1. create a middlewarre for logging the number of requests to the server
@@ -20,12 +19,20 @@ let requestCount = 0;
        You have been given a numberOfRequestsForUser object to start off with which
        clears every one second   
 
+   3.  You have been given an express server which has a few endpoints.
+       Your task is to
+       1. Ensure that if there is ever an exception, the end user sees a status code of 404 --> GLOBAL CATCH (SOLUTION)
+       2. Maintain the errorCount variable whose value should go up every time there is an exception in any endpoint    
+
 
  **/
 
 app.use(ServerRequestCount)
 app.use(rateLimiter)
 
+
+
+let requestCount = 0;
 function ServerRequestCount(req, res, next) {
   requestCount += 1
   next()
@@ -39,7 +46,6 @@ setInterval(() => {
 
 
 function rateLimiter(req, res, next) {
-
   const userId = req.headers["user-id"]
   numberOfRequestsForUser[userId] = (numberOfRequestsForUser[userId] || 0) + 1
   if (numberOfRequestsForUser[userId] < 5) {
@@ -52,17 +58,30 @@ function rateLimiter(req, res, next) {
 }
 
 
+let errorCount = 0
+
+
 app.get('/user', function (req, res) {
   res.status(200).json({ name: 'john', requestCount });
 });
 
-app.post('/user', function (req, res) {
+app.post('/user-error', function (req, res) {
+  throw new Error("this end-point throws an error")
   res.status(200).json({ msg: 'created dummy user', requestCount });
 });
 
 app.get('/requestCount', function (req, res) {
   res.status(200).json({ requestCount });
 });
+
+
+app.use(function (err, req, res, next) {
+  errorCount += 1
+  res.status(404).json({
+    msg: "something is up with the server",
+    ErrorCount: errorCount
+  })
+})
 
 app.listen(3000, () => {
   console.log(`Happy listening on port 3000`)
