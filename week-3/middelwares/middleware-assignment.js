f578e1c53b08ed4ea40a56ff53d34e31020defa2
Amartya <amartya.kumar2001@gmail.com>
2026-03-10 01:02:13 +0530

    chore: did the second part of assignment - rateLimiter

--- week-3/middelwares/middleware-assignment.js
+++ week-3/middelwares/middleware-assignment.js
@@ -11,15 +11,44 @@ let requestCount = 0;
       maintain a count of the number of requests made to the server in the global
       requestCount variable
 
+   2.  You have been given an express server which has a few endpoints.
+       Your task is to create a global middleware (app.use) which will
+       rate limit the requests from a user to only 5 request per second
+       If a user sends more than 5 requests in a single second, the server
+       should block them with a 404.
+       User will be sending in their user id in the header as 'user-id'
+       You have been given a numberOfRequestsForUser object to start off with which
+       clears every one second   
+
 
  **/
 
 app.use(ServerRequestCount)
-
+app.use(rateLimiter)
 
 function ServerRequestCount(req, res, next) {
   requestCount += 1
   next()
+}
+
+
+let numberOfRequestsForUser = {};
+setInterval(() => {
+  numberOfRequestsForUser = {};
+}, 5000)
+
+
+function rateLimiter(req, res, next) {
+
+  const userId = req.headers["user-id"]
+  numberOfRequestsForUser[userId] = (numberOfRequestsForUser[userId] || 0) + 1
+  if (numberOfRequestsForUser[userId] < 5) {
+    next()
+  } else {
+    res.status(404).json({
+      msg: "Sorry you bombarding the server sir"
+    })
+  }
 }
 
 
