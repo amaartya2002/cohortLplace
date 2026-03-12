821135576d7d34005289a29f1604bee10dbef093
Amartya <amartya.kumar2001@gmail.com>
2026-03-12 20:15:25 +0530

    chore: do a express app setup

---
+++ week-3/moongose-assignment-week-4/index.js
@@ -0,0 +1,13 @@
+const express = require("express")
+
+const app = express()
+
+
+app.get("/hello", (req, res) => {
+  res.send("Hello there!")
+})
+
+
+app.listen(3000, () => {
+  console.log(`Listening on port 3000`)
+})
